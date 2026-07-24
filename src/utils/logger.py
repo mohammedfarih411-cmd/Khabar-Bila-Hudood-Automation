b49949646d5c80332bf472e68c6d6aadcd561121
farihm96-hue <farihm96@gmail.com>
2026-07-24 19:38:28 +0200

    Create logger.py

---
+++ src/utils/logger.py
@@ -0,0 +1,33 @@
+import logging
+from pathlib import Path
+
+
+LOG_DIR = Path("logs")
+LOG_DIR.mkdir(exist_ok=True)
+
+
+def setup_logger():
+    logger = logging.getLogger("KhabarBilaHudood")
+
+    if logger.hasHandlers():
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(message)s"
+    )
+
+    file_handler = logging.FileHandler(
+        LOG_DIR / "app.log",
+        encoding="utf-8"
+    )
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
