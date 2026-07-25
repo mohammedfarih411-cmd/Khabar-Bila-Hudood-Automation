a3547388f237bad227cfb44807711af740c7fad8
farihm96-hue <farihm96@gmail.com>
2026-07-25 19:28:53 +0200

    Add script to obtain YouTube OAuth token

---
+++ get_token.py
@@ -0,0 +1,21 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+import json
+
+SCOPES = ["https://www.googleapis.com/auth/youtube.upload"]
+
+client_config = {
+    "installed": {
+        "client_id": "YOUR_CLIENT_ID",
+        "client_secret": "YOUR_CLIENT_SECRET",
+        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+        "token_uri": "https://oauth2.googleapis.com/token"
+    }
+}
+
+flow = InstalledAppFlow.from_client_config(client_config, SCOPES)
+creds = flow.run_local_server(port=0)
+
+print("\n==============================")
+print("YOUTUBE_TOKEN:")
+print(creds.refresh_token)
+print("==============================")
